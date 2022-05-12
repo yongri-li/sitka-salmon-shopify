a9f5061120df2848658abfdbbc7d50f9acaf7d21
yongri-li <yongri.liii+1@gmail.com>
2022-05-12 17:32:40 -0400

    style: add style guide

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,14 @@ export default function Home() {
         />
       </div>
       <h1>Welcome to your Nacelle Storefront</h1>
+      <h2>Welcome to your Nacelle Storefront</h2>
+      <h3>Welcome to your Nacelle Storefront</h3>
+      <h4>Welcome to your Nacelle Storefront</h4>
+      <h5>Welcome to your Nacelle Storefront</h5>
+      <h6>Welcome to your Nacelle Storefront</h6>
+      <p>Welcome to your Nacelle Storefront</p>
+      <a>Welcome to your Nacelle Storefront</a>
+      <a className="btn">Welcome to your Nacelle Storefront</a>
       <p>
         Edit <code className={styles.inlineCode}>pages/index.js</code> to get
         started.
