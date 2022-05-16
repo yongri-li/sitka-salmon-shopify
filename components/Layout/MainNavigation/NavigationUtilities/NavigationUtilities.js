3fd4d4b5e2d0902407c1b3bfa2a27b64b0713268
yongri-li <yongri.liii+1@gmail.com>
2022-05-16 14:48:50 -0700

    feat: fixes hover state for customer service info modal

--- components/Layout/MainNavigation/NavigationUtilities/NavigationUtilities.js
+++ components/Layout/MainNavigation/NavigationUtilities/NavigationUtilities.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useModalContext } from '@/context/ModalContext';
+import { CSSTransition } from 'react-transition-group';
 import Link from 'next/link';
 import IconSearch from '@/svgs/search.svg'
 import IconQuestion from '@/svgs/question-circle.svg'
@@ -9,6 +10,9 @@ import IconCart from '@/svgs/cart.svg'
 const NavigationUtilities = ({props, classes}) => {
 
   const [showCustomerServiceInfo, setShowCustomerServiceInfo] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const nodeRef = useRef(null);
+
   const navCTA = props.nonMemberCta;
   const customerService = props.customerService;
 
@@ -19,6 +23,10 @@ const NavigationUtilities = ({props, classes}) => {
     modalContext.setModalType('create')
     modalContext.setIsOpen(true)
   }
+
+  useEffect(() => {
+    isHovered ? setShowCustomerServiceInfo(true) : setShowCustomerServiceInfo(false)
+  }, [isHovered]);
 
   return (
     <ul className={[classes.navItems, classes.navUtilities].join(' ')}>
@@ -34,27 +42,38 @@ const NavigationUtilities = ({props, classes}) => {
         </button>
       </li>
       <li
-        onMouseEnter={() => setShowCustomerServiceInfo(true)}
-        onMouseLeave={() => setShowCustomerServiceInfo(false)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
         className={classes.navItem}>
           <IconQuestion />
-          {customerService && showCustomerServiceInfo &&
-            <div className={classes.customerServiceInfo}>
-              <div className={classes.customerServiceInfoContent}>
-                <h4>{customerService.header}</h4>
-                <p>{customerService.subheader}</p>
-                <ul>
-                  {customerService.customerServiceNavigation.menuItems.map(item => {
-                    return <li key={item._key}>
-                      <Link href={item.linkUrl ? item.linkUrl : ''}>
-                        <a>{item.linkText}</a>
-                      </Link>
-                    </li>
-                  })}
-                </ul>
+          <CSSTransition in={customerService && showCustomerServiceInfo} timeout={125} nodeRef={nodeRef} unmountOnExit classNames={{
+            'enter': classes.customerServiceInfoEnter,
+            'enterActive': classes.customerServiceInfoEnterActive,
+            'enterDone': classes.customerServiceInfoEnterDone,
+            'exit': classes.customerServiceInfoExit,
+          }}>
+            <div ref={nodeRef} className={classes.customerServiceInfoModal}>
+              <div className={classes.customerServiceInfoOverlay}></div>
+              <div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                className={classes.customerServiceInfo}>
+                <div className={classes.customerServiceInfoContent}>
+                  <h4>{customerService.header}</h4>
+                  <p>{customerService.subheader}</p>
+                  <ul>
+                    {customerService.customerServiceNavigation.menuItems.map(item => {
+                      return <li key={item._key}>
+                        <Link href={item.linkUrl ? item.linkUrl : ''}>
+                          <a>{item.linkText}</a>
+                        </Link>
+                      </li>
+                    })}
+                  </ul>
+                </div>
               </div>
             </div>
-          }
+          </CSSTransition>
       </li>
       <li className={classes.navItem}><IconCart /></li>
     </ul>
