f222482da52d4127bf7a0d7963e1a742412e837a
yongri-li <yongri.liii+1@gmail.com>
2022-05-19 09:34:18 -0700

    feat: adds api to add profile to klaviyo list

--- components/Layout/Footer/FooterEmailSignup/FooterEmailSignup.js
+++ components/Layout/Footer/FooterEmailSignup/FooterEmailSignup.js
@@ -1,13 +1,33 @@
-import {useRef} from 'react'
+import {useRef, useState} from 'react'
 import IconEnvelope from '@/svgs/envelope.svg'
 
 const FooterEmailSignup = ({props, classes}) => {
 
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [showErrorMessage, setShowErrorMessage] = useState(false)
   const emailRef = useRef()
 
   const submitForm = (e) => {
     e.preventDefault()
-    console.log(emailRef.current.value)
+    fetch('/api/klaviyo/klaviyo-add-to-list', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        list_id: 'MuPGVv',
+      })
+    })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.message === 'success') {
+        setShowSuccessMessage(true)
+        setShowErrorMessage(false)
+      } else {
+        setShowErrorMessage(true)
+      }
+    })
   }
 
   return (
@@ -17,13 +37,20 @@ const FooterEmailSignup = ({props, classes}) => {
         <p className={classes.footerEmailDescription}>{props.description}</p>
       </div>
       <div className={[classes.footerBlock, classes.footerEmailFormContainer].join(' ')}>
-        <form onSubmit={(e) => submitForm(e)} className={classes.footerEmailForm}>
-          <div className="input-group">
-            <input className="input" name="email" type="email" placeholder="email address" ref={emailRef} />
-            <IconEnvelope />
-          </div>
-          <button className="btn salmon">{props.ctaText}</button>
-        </form>
+        {showSuccessMessage ? (
+          <p className={classes.footerEmailTextSuccess}>Thank you for subscribing! Check your inbox!</p>
+        ):(
+          <form onSubmit={(e) => submitForm(e)} className={classes.footerEmailForm}>
+            <div className="input-group">
+              <input className="input" name="email" type="email" placeholder="email address" ref={emailRef} />
+              <IconEnvelope />
+            </div>
+            <button className="btn salmon">{props.ctaText}</button>
+            {showErrorMessage &&
+              <p className={classes.footerEmailTextError}>Sorry, an error has occurred. Please try again or email salmonsupport@sitkasalmonshares.com for assistance</p>
+            }
+          </form>
+        )}
       </div>
     </div>
   )
