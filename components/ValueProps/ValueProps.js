319d5534445a4baa57e4214a923c68fc700c4fd1
yongri-li <yongri.liii+1@gmail.com>
2022-05-19 23:09:05 -0400

    style: add container on value props

--- components/ValueProps/ValueProps.js
+++ components/ValueProps/ValueProps.js
@@ -14,41 +14,43 @@ const ValueProps = ({ fields }) => {
   }
 
   return (
-    <div className={`${classes['props']} ${classes[fields.backgroundColor]} container`}>
-        <div className={classes['props__text']}>
-            {fields.header && <h1>{fields.header}</h1>}
-            {fields.subheader && <h2>{fields.subheader}</h2>}
-        </div>
-        <div className={`${classes['props__row']} ${classes[extraPropsClass]}`}>
-           {valueProps.map((valueProp) => {
-               return (
-                    <div className={`${classes['props__col']} ${classes[extraPropsClass]}`} key={valueProp._key}>
-                        {valueProp.propImage.asset.url && 
-                        <div className={classes['props__col-wrap']}>
-                            <div className={classes['props__col-img']}>
-                                <Image
-                                    src={valueProp.propImage.asset.url}
-                                    layout="fill"
-                                    objectFit='contain'
-                                    objectPosition={'center'}
-                                />
+    <div className={`${classes['props']} ${classes[fields.backgroundColor]}`}>
+        <div className="container">
+            <div className={classes['props__text']}>
+                {fields.header && <h1>{fields.header}</h1>}
+                {fields.subheader && <h2>{fields.subheader}</h2>}
+            </div>
+            <div className={`${classes['props__row']} ${classes[extraPropsClass]}`}>
+            {valueProps.map((valueProp) => {
+                return (
+                        <div className={`${classes['props__col']} ${classes[extraPropsClass]}`} key={valueProp._key}>
+                            {valueProp.propImage.asset.url && 
+                            <div className={classes['props__col-wrap']}>
+                                <div className={classes['props__col-img']}>
+                                    <Image
+                                        src={valueProp.propImage.asset.url}
+                                        layout="fill"
+                                        objectFit='contain'
+                                        objectPosition={'center'}
+                                    />
+                                </div>
+                            </div>
+                            }
+                            <div className={classes['props__col--text']}>
+                                {valueProp.propHeader && <h1 className="heading--prop">{valueProp.propHeader}</h1>}
+                                {valueProp.propSubheader && <p className="secondary--body">{valueProp.propSubheader}</p>}
                             </div>
                         </div>
-                        }
-                        <div className={classes['props__col--text']}>
-                            {valueProp.propHeader && <h1 className="heading--prop">{valueProp.propHeader}</h1>}
-                            {valueProp.propSubheader && <p className="secondary--body">{valueProp.propSubheader}</p>}
-                        </div>
-                    </div>
-               )
-           })}
-        </div>
+                )
+            })}
+            </div>
 
-        {fields.ctaUrl && <Link href={`${fields.ctaUrl}`}>
-            <a className={`${classes['props__btn']} salmon btn text-align--center no-underline`}>
-            {fields.ctaText}
-            </a>
-        </Link>}
+            {fields.ctaUrl && <Link href={`${fields.ctaUrl}`}>
+                <a className={`${classes['props__btn']} salmon btn text-align--center no-underline`}>
+                {fields.ctaText}
+                </a>
+            </Link>}
+        </div>
     </div>
   );
 };
