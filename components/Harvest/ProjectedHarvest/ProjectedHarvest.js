716139a60c76ac28b849e5da70e4af095221da30
yongri-li <yongri.liii+1@gmail.com>
2022-06-02 23:07:27 -0400

    refactor: remove unnecessary imports

--- components/Harvest/ProjectedHarvest/ProjectedHarvest.js
+++ components/Harvest/ProjectedHarvest/ProjectedHarvest.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import Image from 'next/image'
 
 import "swiper/css"
 import classes from './ProjectedHarvest.module.scss'
