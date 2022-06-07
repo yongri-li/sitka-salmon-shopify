a31f2d65ed36461bcc863a3c17b19ba9cd334da3
yongri-li <yongri.liii+1@gmail.com>
2022-06-07 10:18:14 -0400

    refactor: lowercase months

--- components/Harvest/GlobalSampler/GlobalSampler.js
+++ components/Harvest/GlobalSampler/GlobalSampler.js
@@ -13,7 +13,7 @@ const GlobalSampler = ({ fields }) => {
 
   useEffect(() => {
      // make reusable
-    const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const month = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
     const date = new Date()
     const monthName = month[date.getMonth()]
 
