d4632d7cf11f1a2024bde02ab49415680715a7b1
yongri-li <yongri.liii+1@gmail.com>
2022-06-30 12:08:03 -0400

    refactor: conditional play button rendering

--- components/ArticleCard/ArticleCard.js
+++ components/ArticleCard/ArticleCard.js
@@ -2,16 +2,22 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import IconBullet from '@/svgs/list-item.svg'
+import PlayIcon from '@/svgs/play.svg'
 
 import classes from "./ArticleCard.module.scss"
 
 const ArticleCard = ({ article, reverse }) => {
+  const tags = article.articleTags
+  const foundTag = tags?.find(tag => tag === 'video' || 'live cooking class')
   return (
     <Link href={`/${article.handle.current}`}>
         <a className={classes['article-card']}>
             <div className={`${classes['slider__slide']} ${reverse ? classes['row'] : ''}`}>
                 {article.heroImage && <div className={classes['image-wrap']}>
                     <Image width={438} height={600} alt={article.alt} src={article.heroImage.asset.url} />
+                    {foundTag && <div className={classes['play']}>
+                        <PlayIcon />
+                    </div>}
                 </div>}
                 <div className={classes['text']}>
                     {article.heroHeader && <h4 className='heading--article'>{article.heroHeader}</h4>}
