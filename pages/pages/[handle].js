130a9e7b6fb270d94a4ccecd8613f88402bc42ac
yongri-li <yongri.liii+1@gmail.com>
2022-07-13 15:01:14 -0700

    feat: adds dynamic page route

---
+++ pages/pages/[handle].js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { nacelleClient } from 'services'
+import ContentSections from '@/components/Sections/ContentSections'
+
+export default function DynamicPage({ page }) {
+  return (
+    <>
+      <ContentSections sections={page.fields.content} />
+    </>
+  )
+}
+
+export async function getStaticPaths({ params }) {
+  const pages = await nacelleClient.content({
+    handles: ['page']
+  })
+
+  const handles = pages.map((page) => ({ params: { handle: page.handle } }))
+
+  return {
+    paths: handles,
+    fallback: 'blocking'
+  }
+}
+
+export async function getStaticProps({ params }) {
+
+  const pages = await nacelleClient.content({
+    handles: [params.handle]
+  })
+
+  if (!pages.length) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      page: pages[0]
+    }
+  }
+
+}
