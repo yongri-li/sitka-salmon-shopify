7f36ae8d58915b865e47f7fff261d8cc2bc1025f
yongri-li <yongri.liii+1@gmail.com>
2022-07-18 12:06:58 -0700

    feat: adds social sharing icons

--- components/Article/ArticleSocialSharing/ArticleSocialSharing.js
+++ components/Article/ArticleSocialSharing/ArticleSocialSharing.js
@@ -5,6 +5,10 @@ import IconThreeDotsCircle from '@/svgs/three-dots-circle.svg'
 import IconPrinter from '@/svgs/printer.svg'
 import IconBookmark from '@/svgs/bookmark.svg'
 import IconShare from '@/svgs/share.svg'
+import IconShareFacebook from '@/svgs/share-facebook.svg'
+import IconSharePinterest from '@/svgs/share-pinterest.svg'
+import IconShareEmail from '@/svgs/share-email.svg'
+import IconShareLink from '@/svgs/share-link.svg'
 import { useArticleContext } from '@/context/ArticleContext'
 
 const ArticleSocialSharing = () => {
@@ -28,18 +32,41 @@ const ArticleSocialSharing = () => {
         </button>
       </li> */}
       <li>
-        <button>
-          <IconShare /><span>Share</span>
-        </button>
+        <div className={classes['article-social-sharing-btn']}>
+          <IconShare />
+          <span>Share</span>
+          <ul className="">
+            <li>
+              <button>
+                <IconShareFacebook />
+              </button>
+            </li>
+            <li>
+              <button>
+                <IconSharePinterest />
+              </button>
+            </li>
+            <li>
+              <button>
+                <IconShareEmail />
+              </button>
+            </li>
+            <li>
+              <button>
+                <IconShareLink />
+              </button>
+            </li>
+          </ul>
+        </div>
       </li>
       {isMobile && mounted && <li>
         <button onClick={() => setIsSidebarOpen(true)}
-          className={classes['more-info-btn']}>
+          className={`${classes['article-social-sharing-btn']} ${classes['more-info-btn']}`}>
           <IconThreeDotsCircle /><span>More Info</span>
         </button>
       </li>}
       {isDesktop && mounted && <li>
-        <button onClick={() => window.print()}>
+        <button  className={classes['article-social-sharing-btn']} onClick={() => window.print()}>
           <IconPrinter /><span>Print</span>
         </button>
       </li>}
