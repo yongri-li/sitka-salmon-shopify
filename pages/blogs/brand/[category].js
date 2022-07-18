005be10636abf5442de165be22a9908b5ab5cca8
yongri-li <yongri.liii+1@gmail.com>
2022-07-18 00:45:04 -0700

    feat: adds dynamic routes for blogs and articles

--- pages/blogs/brand/[category].js
+++ pages/blogs/brand/[category].js
@@ -3,6 +3,10 @@ import { nacelleClient } from 'services'
 import ListingsTemplate from '@/components/Blog/BlogListings/ListingsTemplate'
 
 const BrandBlogListings = ({ articles, blogSettings, page }) => {
+
+  console.log("articles:", articles)
+
+
   return (
     <ListingsTemplate articles={articles} blogSettings={blogSettings} page={page} />
   )
@@ -14,11 +18,11 @@ export async function getStaticPaths() {
     const blogs = await nacelleClient.content({
       type: 'blogs'
     })
-  
+
     const validBlogs = blogs.filter(blog => blog.fields.blogType === 'brand')
-  
+
     const handles = validBlogs.map((article) => ({ params: { handle: article.handle } }))
-  
+
     return {
       paths: handles,
       fallback: 'blocking'
@@ -26,25 +30,26 @@ export async function getStaticPaths() {
   }
 
 export async function getStaticProps({ params }) {
+
   const articles = await nacelleClient.content({
     type: 'standardArticle'
   })
 
-  const validArticles = articles.filter(article => article.fields.blog.handle.current === params.handle)
+  const validArticles = articles.filter(article => article.fields.blog.handle.current === params.category)
 
   const blogSettings = await nacelleClient.content({
     type: 'blogSettings'
   })
 
   const pages  = await nacelleClient.content({
-    handles: [params.handle],
+    handles: [params.category],
     type: 'blog'
   })
 
   if (!articles.length) {
     return {
       notFound: true
-    }  
+    }
   }
 
   return {
