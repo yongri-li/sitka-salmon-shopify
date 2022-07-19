777d6c8adb0498e13de16dd79e22907c73d8bccd
yongri-li <yongri.liii+1@gmail.com>
2022-07-19 14:43:57 -0500

    davis API endpoint example

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useCustomerContext } from '@/context/CustomerContext'
 import classes from './AccountMainPage.module.scss'
 
 export default function AccountMainPage() {
 
+  const retrieveSubs = async (id) => {
+    useEffect(() => {
+      fetch('/api/account/get-subs?cID='+id, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          // TODO:
+          // 'Authorization': `Bearer test`,
+        }
+      })
+      .then(res => res.json())
+      .then(res => {
+        if (res.message === 'success') {
+          const results = res.data
+          console.log('get-subs',results)
+
+        }
+      })
+    }, [])
+  };
+
+  
+
   const customerContext = useCustomerContext();
   console.log(`customerContext: `, customerContext);
-  console.log(customerContext.customer?.displayName ?? 'NOT LOADED');
+
+  {customerContext.customer?.id &&
+    console.log(customerContext.customer?.displayName ?? 'NOT LOADED');
+    retrieveSubs(customerContext.customer?.id.substring(100, 23));
+  }
+  
   return (<div className={`${classes['main']}`}><div className={`${classes['greeting']}`}>Welcome to the account page, {customerContext.customer?.displayName ?? 'LOADING...'}</div></div>)
 }
