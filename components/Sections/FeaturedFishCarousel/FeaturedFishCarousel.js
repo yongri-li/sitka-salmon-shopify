5fd46575bfdd3491dcabdd4ea603f2ed06e90c58
yongri-li <yongri.liii+1@gmail.com>
2022-07-22 15:27:20 -0700

    feat: adds api version for sanity and improves ui for feature fish slider

--- components/Sections/FeaturedFishCarousel/FeaturedFishCarousel.js
+++ components/Sections/FeaturedFishCarousel/FeaturedFishCarousel.js
@@ -15,6 +15,8 @@ const FeaturedFishCarousel = ({fields}) => {
   const [swiper, setSwiper] = useState()
 
   const sliderRef = useRef(null);
+  const prevSlideBtnRef = useRef()
+  const nextSlideBtnRef = useRef()
 
   const handlePrev = useCallback(() => {
     if (!sliderRef.current) return;
@@ -25,10 +27,6 @@ const FeaturedFishCarousel = ({fields}) => {
     if (!sliderRef.current) return;
     sliderRef.current.swiper.slideNext();
   }, []);
-
-  useEffect(() => {
-    console.log("Swiper:", swiper)
-  }, [swiper])
 
   function urlFor(source) {
     return builder.image(source)
@@ -42,8 +40,8 @@ const FeaturedFishCarousel = ({fields}) => {
         <h2 className="h1">{header}</h2>
         <p>{subheader}</p>
         {featuredFishes.length >= 4 && <div className={classes['feature-fish-carousel__nav-btns']}>
-          <button className={classes['feature-fish-carousel__nav-btn']} onClick={handlePrev}><IconArrowLeft /></button>
-          <button className={classes['feature-fish-carousel__nav-btn']} onClick={handleNext}><IconArrowLeft /></button>
+          <button className={classes['feature-fish-carousel__nav-btn']} onClick={handlePrev} ref={prevSlideBtnRef}><IconArrowLeft /></button>
+          <button className={classes['feature-fish-carousel__nav-btn']} onClick={handleNext} ref={nextSlideBtnRef}><IconArrowLeft /></button>
         </div>}
       </div>
       {featuredFishes.length > 0 &&
@@ -53,21 +51,37 @@ const FeaturedFishCarousel = ({fields}) => {
           spaceBetween={40}
           threshold={2}
           onSwiper={setSwiper}
+          onProgress={({progress}) => {
+            if (progress <= 0) {
+              prevSlideBtnRef.current.style.opacity = 0.3
+              prevSlideBtnRef.current.style.pointerEvents = 'none'
+              nextSlideBtnRef.current.style.opacity = 1
+              nextSlideBtnRef.current.style.pointerEvents = 'auto'
+            } else if (progress >= 1) {
+              prevSlideBtnRef.current.style.opacity = 1
+              prevSlideBtnRef.current.style.pointerEvents = 'auto'
+              nextSlideBtnRef.current.style.opacity = 0.3
+              nextSlideBtnRef.current.style.pointerEvents = 'none'
+            } else {
+              prevSlideBtnRef.current.style.opacity = 1
+              prevSlideBtnRef.current.style.pointerEvents = 'auto'
+              nextSlideBtnRef.current.style.opacity = 1
+              nextSlideBtnRef.current.style.pointerEvents = 'auto'
+            }
+          }}
           className={classes['featured-fish-carousel__item-list']}>
             {featuredFishes.map(item => {
 
               const { header, peakSeason, image } = item
               const cropImageUrl = urlFor(image).width(438).height(600).focalPoint(image.hotspot.x, image.hotspot.y).crop('focalpoint').fit('crop').url()
 
-              {/* const imageInlineStyles = {
-                'filter': `brightness(${imageBrightness ? imageBrightness : 100}%)`
-              } */}
-
-              {/* console.log("item:", item) */}
+              const imageInlineStyles = {
+                'filter': `brightness(${image?.imageBrightness ? image.imageBrightness : 100}%)`
+              }
 
               return <SwiperSlide className={classes['featured-fish-carousel__item']} key={item._id}>
                 <div className={classes['featured-fish-carousel__item-image']}>
-                  <ResponsiveImage src={cropImageUrl} alt={image.asset.alt || ''} />
+                  <ResponsiveImage src={cropImageUrl} alt={image.asset.alt || ''}  style={imageInlineStyles} />
                 </div>
                 <div className={classes['feature-fish-carousel__content']}>
                   <h2 className="h4">{header}</h2>
--- services/sanityClient.js
+++ services/sanityClient.js
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
+  apiVersion: '2021-08-31',
   useCdn: true
 })
