7952df9d950270b0a53a1d2208cb4e3edd21bab0
yongri-li <yongri.liii+1@gmail.com>
2022-07-25 23:10:47 -0500

    Clean up the code

--- pages/account/index.js
+++ pages/account/index.js
@@ -3,37 +3,43 @@ import { useCustomerContext } from '@/context/CustomerContext'
 import classes from './AccountMainPage.module.scss'
 
 export default function AccountMainPage() {
+  const customerContext = useCustomerContext()
+  console.log(`customerContext: `, customerContext)
 
-  const retrieveSubs = async (id) => {
-    useEffect(() => {
-      fetch('/api/account/get-subs?cID='+id, {
+  useEffect(() => {
+    console.log('running effect with customer ', customerContext.customer)
+    if (customerContext.customer?.id) {
+      const idArr = customerContext.customer.id.split('/');
+      const id = idArr[idArr.length - 1];
+      fetch('/api/account/get-subs?cID=' + id, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           // TODO:
           // 'Authorization': `Bearer test`,
-        }
+        },
       })
-      .then(res => res.json())
-      .then(res => {
-        if (res.message === 'success') {
-          const results = res.data
-          console.log('get-subs',results)
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.message === 'success') {
+            const results = res.data
+            console.log('get-subs', results)
+          }
+        })
+    }
+  }, [customerContext.customer])
 
-        }
-      })
-    }, [])
-  };
+  // {customerContext.customer?.id &&
+  //   console.log(customerContext.customer?.displayName ?? 'NOT LOADED');
+  //   retrieveSubs(customerContext.customer?.id.substring(100, 23));
+  // }
 
-  
-
-  const customerContext = useCustomerContext();
-  console.log(`customerContext: `, customerContext);
-
-  {customerContext.customer?.id &&
-    console.log(customerContext.customer?.displayName ?? 'NOT LOADED');
-    retrieveSubs(customerContext.customer?.id.substring(100, 23));
-  }
-  
-  return (<div className={`${classes['main']}`}><div className={`${classes['greeting']}`}>Welcome to the account page, {customerContext.customer?.displayName ?? 'LOADING...'}</div></div>)
+  return (
+    <div className={`${classes['main']}`}>
+      <div className={`${classes['greeting']}`}>
+        Welcome to the account page,{' '}
+        {customerContext.customer?.displayName ?? 'LOADING...'}
+      </div>
+    </div>
+  )
 }
--- pages/api/account/get-subs.js
+++ pages/api/account/get-subs.js
@@ -19,8 +19,8 @@ export default async function handler(req, res) {
     console.log(response.data);
     res.status(201).json({ message: 'success', data: response.data });
   })
-  .catch((err) => {
+  .catch((error) => {
     console.log(error);
-    res.status(400).json({ message: 'error', data: err });
+    res.status(400).json({ message: 'error', data: error });
   });
 }
