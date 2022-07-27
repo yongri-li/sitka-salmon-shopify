0ec7a4773fc4eaeacd76b1c52663b9f79c7f1baf
yongri-li <yongri.liii+1@gmail.com>
2022-07-26 17:39:24 -0700

    feat: adds bottom content and adds more components to wysiwyg section

--- components/Sections/WYSIWYG/WYSIWYG.js
+++ components/Sections/WYSIWYG/WYSIWYG.js
@@ -1,16 +1,39 @@
 import React from 'react'
 import classes from './WYSIWYG.module.scss'
 import { PortableText } from '@portabletext/react'
+import Link from 'next/link'
+import Video from '@/components/Video'
 
 const WYSIWYG = ({fields}) => {
 
   const myPortableTextComponents = {
+    marks: {
+      link: ({children, value}) => {
+        const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined
+        if (value.href.includes('mailto')) {
+          return <a href={value.href} target="_blank">{children}</a>
+        }
+        return (
+          <Link href={value.href}>
+            <a rel={rel}>{children}</a>
+          </Link>
+        )
+      }
+    },
     listItem: {
       bullet: ({children}) => {
         return (
           <li className="body">{children}</li>
         )
+      },
+      number: ({children}) => {
+        return (
+          <li className="body">{children}</li>
+        )
       }
+    },
+    types: {
+      youtubeVideoBlock: ({value}) => <Video youtubeVideoId={value.youtubeVideoId} autoplay={false} startVideo="true" className={classes['wysiwyg__video']} />
     }
   }
 
--- pages/pages/[handle].js
+++ pages/pages/[handle].js
@@ -1,26 +1,64 @@
+import React, { useState, useEffect } from 'react'
 import PageSEO from '@/components/SEO/PageSEO'
 import { nacelleClient } from 'services'
 import ContentSections from '@/components/Sections/ContentSections'
 import classes from './Page.module.scss'
+import { useMediaQuery } from 'react-responsive'
+import ResponsiveImage from '@/components/ResponsiveImage'
 
 export default function DynamicPage({ page }) {
   console.log("page:", page)
+
+  const [mounted, setMounted] = useState(false)
+  const isMobile = useMediaQuery({ query: '(max-width: 1073px)' })
+  const isDesktop = useMediaQuery(
+    {query: '(min-width: 1074px)'}
+  )
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
   if (page.type === 'infoPage') {
-    const { header, leftContent , rightContent } = page.fields
+    const { header, leftContent , rightContent, bottomContent, desktopIllustration, mobileIllustration } = page.fields
     return (
-      <div className={`${classes['info-page']} info-page container`}>
-        <div className={classes['info-page__left']}>
-          <h1>{header}</h1>
-          {leftContent &&
-            <ContentSections sections={leftContent} />
-          }
+      <>
+        <div className={`${classes['info-page']} info-page container`}>
+          <PageSEO seo={page.seo} />
+          <div className={classes['info-page__left']}>
+            <h1 className={classes['info-page__header']}>{header}</h1>
+            {leftContent &&
+              <ContentSections sections={leftContent} />
+            }
+            {desktopIllustration && isDesktop && mounted &&
+              <div className={classes['info-page__illustration-image']}>
+                <ResponsiveImage
+                  src={desktopIllustration.asset.url}
+                  layout="fill"
+                  alt={desktopIllustration.asset.alt || ''}
+                />
+              </div>
+            }
+            {mobileIllustration && isMobile && mounted &&
+              <div className={classes['info-page__illustration-image']}>
+                <ResponsiveImage
+                  src={mobileIllustration.asset.url}
+                  layout="fill"
+                  alt={mobileIllustration.asset.alt || ''}
+                />
+              </div>
+            }
+          </div>
+          <div className={classes['info-page__right']}>
+            {rightContent &&
+              <ContentSections sections={rightContent} />
+            }
+          </div>
         </div>
-        <div className={classes['info-page__right']}>
-          {rightContent &&
-            <ContentSections sections={rightContent} />
-          }
-        </div>
-      </div>
+        {bottomContent &&
+          <ContentSections sections={bottomContent} />
+        }
+      </>
     )
   }
 
