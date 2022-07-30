ea9f5f11649d8d6e1e0b28ac313157fa496a0478
yongri-li <yongri.liii+1@gmail.com>
2022-07-29 21:25:49 -0700

    feat: adds api version for sanity client

--- services/sanityClient.js
+++ services/sanityClient.js
@@ -2,6 +2,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: 'production', 
+  dataset: 'production',
+  apiVersion: '2021-08-31',
   useCdn: true
 })
