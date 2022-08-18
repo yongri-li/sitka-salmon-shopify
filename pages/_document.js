31a5cff4dfb1ec9daf776e2d902d33a4cd8094ba
yongri-li <yongri.liii+1@gmail.com>
2022-08-18 15:34:52 -0700

    feat: adds gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { TrackingHeadScript } from "@phntms/react-gtm"
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head>
+          <TrackingHeadScript id={process.env.NEXT_PUBLIC_GA_TRACKING_ID} />
           <script async type="text/javascript" data-api-key={process.env.NEXT_PUBLIC_STAMPEDIO_KEY_PUBLIC} id="stamped-script-widget" src="https://cdn1.stamped.io/files/widget.min.js"></script>
         </Head>
         <body>
@@ -13,8 +15,8 @@ class MyDocument extends Document {
           <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=PX38fc"></script>
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
