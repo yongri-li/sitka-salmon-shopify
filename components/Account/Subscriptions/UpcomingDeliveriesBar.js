6715da05c1e079cac9b41cffc9514aeaf9a1db20
yongri-li <yongri.liii+1@gmail.com>
2022-08-23 21:33:01 -0500

    update styling

--- components/Account/Subscriptions/UpcomingDeliveriesBar.js
+++ components/Account/Subscriptions/UpcomingDeliveriesBar.js
@@ -4,7 +4,7 @@ import classes from './UpcomingDeliveriesBar.module.scss'
 export default function UpcomingDeliveriesBar({subscription}) {
   return <div className={classes['upcoming-deliveries-bar']}>
     <h4 className={classes['section-text']}>Upcoming Deliveries</h4>
-    <div className={classes['shipping-link']}>Shipping to {subscription.subscription_address.street1}</div>
-    <div className={classes['schedule-link']}>Edit Schedule</div>
+    <div className={`${classes['shipping-link']} ${classes['right-link']}`}>Shipping to {subscription.subscription_address.street1}</div>
+    <div className={`${classes['schedule-link']} ${classes['right-link']}`}><a>Edit Schedule</a></div>
   </div>
 }
