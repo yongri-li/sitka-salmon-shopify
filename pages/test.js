ee753b7eb18c7791759a21cb35df56cdb45b220b
yongri-li <yongri.liii+1@gmail.com>
2022-08-26 13:42:03 -0500

    sentry pkg installed and configured

--- pages/test.js
+++ pages/test.js
@@ -3,9 +3,23 @@ import Link from 'next/link';
 
 const test = () => {
   return (
+
+
     <div>test
       <Link href="/">home</Link>
+      <button
+        type="button"
+        onClick={() => {
+          throw new Error("Sentry Frontend Error");
+        }}
+      >
+        Throw error
+      </button>
+
     </div>
+
+
+
   )
 }
 
