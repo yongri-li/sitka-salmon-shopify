cf8efcbb5584cd11adb8fe2d4d78a95b983c095d
yongri-li <yongri.liii+1@gmail.com>
2022-09-09 10:46:48 -0500

    Hide skipped months

--- components/Account/Subscriptions/SubDetail.js
+++ components/Account/Subscriptions/SubDetail.js
@@ -19,20 +19,25 @@ const getHarvetHandle = (variant, product) => {
   }
 }
 
-export default function SubDetail({ subscription, product, variant, membership }) {
+export default function SubDetail({
+  subscription,
+  product,
+  variant,
+  membership,
+}) {
   const [harvest, setHarvest] = useState(null)
   useEffect(() => {
     const getHarvest = async () => {
       const handleValue = getHarvetHandle(product, variant)
-      console.log('got harvest handle value', handleValue);
+      console.log('got harvest handle value', handleValue)
 
       if (!!handleValue) {
-        console.log('trying to get harvest content from nacelle');
+        console.log('trying to get harvest content from nacelle')
         const harvestContent = await nacelleClient.content({
           handles: [handleValue],
           type: 'harvest',
         })
-        console.log('get harvestContent', harvestContent);
+        console.log('get harvestContent', harvestContent)
         if (!!harvestContent) {
           setHarvest(harvestContent[0])
         }
@@ -45,19 +50,39 @@ export default function SubDetail({ subscription, product, variant, membership }
 }
 
 const renderMonths = (subscription, harvest, membership) => {
+  const currentMonth = harvest.fields.months.find((m) =>
+    m.month.includes(subscription.fulfill_month),
+  )
 
-  const currentMonth = harvest.fields.months.find((m) => m.month.includes(subscription.fulfill_month));
-
-  return <div>
-    <UpcomingDeliveriesBar subscription={subscription}/>
-    {currentMonth && <CurrentMonthHarvestDetail subscription={subscription} month={currentMonth} membership={membership}/>}
-    {subscription.group_schedule.map((g) => {
-    // Get the harvest details for the month
-    const month = harvest.fields.months.find((m) => m.month.includes(g.month))
-    return month ? (
-      <FutureMonthHarvest key={`future-harvest-${month.month}`} subscriptionGroupSchedule={g} month={month}/>
-    ) : (
-      <div key={`no-harvest-data-${g.month}`}>No harvest data for {g.month}</div>
-    )
-  })}</div>
+  return (
+    <div>
+      <UpcomingDeliveriesBar subscription={subscription} />
+      {currentMonth && (
+        <CurrentMonthHarvestDetail
+          subscription={subscription}
+          month={currentMonth}
+          membership={membership}
+        />
+      )}
+      {subscription.group_schedule
+        .filter((g) => g.scheduled_status !== 'skipped')
+        .map((g) => {
+          // Get the harvest details for the month
+          const month = harvest.fields.months.find((m) =>
+            m.month.includes(g.month),
+          )
+          return month ? (
+            <FutureMonthHarvest
+              key={`future-harvest-${month.month}`}
+              subscriptionGroupSchedule={g}
+              month={month}
+            />
+          ) : (
+            <div key={`no-harvest-data-${g.month}`}>
+              No harvest data for {g.month}
+            </div>
+          )
+        })}
+    </div>
+  )
 }
